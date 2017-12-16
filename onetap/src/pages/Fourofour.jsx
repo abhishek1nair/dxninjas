fa93e836e34e95b2f7bc5d6eeba39a5ec32306f0
Pranjal Kumar <pranjal.kr94@gmail.com>
2017-12-16 16:23:10 +0530

    barebones all routes

---
+++ onetap/src/pages/Fourofour.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Fourofour extends Component {
+  render() {
+    return (
+      <div>
+        <div className="fourofour">
+          404!
+        </div>
+        <div className="fourofourdescription">
+          Oops! You Did It Again...
+        </div>
+      </div>
+    );
+  }
+}
---
+++ onetap/src/pages/appointmentcreatepage/AppointmentPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AppointmentPage extends Component {
+  render() {
+    return (
+      <div>
+        AppointmentPage
+      </div>
+    );
+  }
+}
