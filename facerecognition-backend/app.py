d7ea998521aa59f92152f3f17ceca62743371cfc
aayushiku <aayushi.k@practo.com>
2017-12-17 04:04:12 +0530

    Adding string

--- facerecognition-backend/app.py
+++ facerecognition-backend/app.py
@@ -112,10 +112,8 @@ def create_appointment():
 
         return 'Done'
     elif request.method == 'GET':
-        print request.args
         face_ids = request.args.getlist('face_ids')
         users = get_user_by_face(face_ids)
-        print users
         ids_string = ''.join(str(x.get('id')) for x in users)
         query = "SELECT * from appointments where user_id in (%s) order by appointment_time"
         cur.execute(query, (ids_string,))
@@ -175,11 +173,9 @@ def get_user_by_contact(contact):
 def get_user_by_face(face_ids):
     db = mysql.connection
     cur = db.cursor()
-    face_param = ','.join(str(x) for x in face_ids)
+    face_param = ','.join(("'" + str(x) + "'") pfor x in face_ids)
     query = "SELECT * from user_profiles as u join user_face_id as uf on u.id=uf.user_id where face_id in ({face_param})".format(
         face_param=face_param)
-    print query
-    params = (face_param,)
     cur.execute(query)
 
     return dictfetchall(cur)
