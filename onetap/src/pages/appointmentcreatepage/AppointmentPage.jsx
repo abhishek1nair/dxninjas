bbc1674ed39dda6c89eb40e0f979e109c3664885
Pranjal Kumar <pranjal.kr94@gmail.com>
2017-12-16 17:37:21 +0530

    form layout for appoint page

--- onetap/src/pages/appointmentcreatepage/AppointmentPage.jsx
+++ onetap/src/pages/appointmentcreatepage/AppointmentPage.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete } from 'antd';
 import Webcam from 'react-webcam';
 
+import '../../AppointmentPage.css';
+
 const FormItem = Form.Item;
 const Option = Select.Option;
 const AutoCompleteOption = AutoComplete.Option;
@@ -251,16 +253,45 @@ RegistrationForm.propTypes = {
 const WrappedRegistrationForm = Form.create()(RegistrationForm);
 
 export default class AppointmentPage extends Component {
+
+  constructor() {
+    super();
+    this.capture = this.capture.bind(this);
+    this.state = {
+      screenshot: null
+    };
+  }
+
+  capture() {
+    const screenshot = this.iWebcam.getScreenshot();
+    this.setState({ screenshot });
+  }
+
   render() {
     return (
-      <div>
+      <div className='c-appointment__wrapper'>
         <Row>
           <Col span={2}/>
-          <Col span={10}>
-            Web cam Comes here!
-          </Col>
-          <Col span={10}>
-            <WrappedRegistrationForm/>
+          <Col span={20}>
+            <div className='c-appointment__title'>
+              Book Appointment
+            </div>
+            <div>
+              <Col span={10}>
+                <Webcam
+                  audio={false}
+                  height={320}
+                  ref={(f) => { this.iWebcam = f; } }
+                  screenshotFormat="image/jpeg"
+                  width={400}
+                />
+                <Button style={{ marginBottom: '20px' }} onClick={this.capture} type="primary">Capture</Button>
+                {this.state.screenshot ? <img alt='captured' src={this.state.screenshot} /> : null}
+              </Col>
+              <Col span={10}>
+                <WrappedRegistrationForm/>
+              </Col>
+            </div>
           </Col>
           <Col span={2}/>
         </Row>
