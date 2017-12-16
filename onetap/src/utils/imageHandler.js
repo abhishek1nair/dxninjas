c84a6f91865aca115871658005c0e4353607e28e
abhishek1nair <abhisheknair@practo.com>
2017-12-16 20:31:44 +0530

    add wrappers over kairos api

---
+++ onetap/src/utils/imageHandler.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import constants from '../constants/constants';
+
+const DEFAULT_GALLERY = 'practolense';
+
+export const enroll = ({
+  image,
+  subjectId,
+  galleryName = DEFAULT_GALLERY
+}) => axios({
+  method: 'POST',
+  url: 'https://api.kairos.com/enroll',
+  headers: {
+    app_id: constants.KAIROS_APP_ID,
+    app_key: constants.KAIROS_APP_KEY
+  },
+  data: {
+    image,
+    subject_id: subjectId,
+    gallery_name: galleryName
+  }
+});
+
+export const recognize = ({
+  image,
+  galleryName = DEFAULT_GALLERY
+}) => axios({
+  method: 'POST',
+  url: 'https://api.kairos.com/recognize',
+  headers: {
+    app_id: constants.KAIROS_APP_ID,
+    app_key: constants.KAIROS_APP_KEY
+  },
+  data: {
+    image,
+    gallery_name: galleryName
+  }
+});
+
+window.kairos = {
+  enroll,
+  recognize
+};
+
+export default {};
